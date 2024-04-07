7590ca3cdc4e7ef52d3907add8e710a9f4315d27
Maia <matteusmaia889@gmail.com>
2024-04-07 19:49:13 -0300

    feat: base of App.jsx

--- frontend_react/src/App.jsx
+++ frontend_react/src/App.jsx
@@ -1,5 +1,16 @@
 import "./index.css";
 
+import { Testemonial, About, Footer, Header, Skils, Work } from "./container";
+
 export function App() {
-  return <>App</>;
+  return (
+    <div className="app">
+      <Header />
+      <About />
+      <Work />
+      <Skils />
+      <Testemonial />
+      <Footer />
+    </div>
+  );
 }
