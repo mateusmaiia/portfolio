b365aab2e76eecfc6faf2868413350321ce0c6fc
Maia <matteusmaia889@gmail.com>
2024-04-07 20:13:30 -0300

    feat: assets files

--- frontend_react/src/App.jsx
+++ frontend_react/src/App.jsx
@@ -1,10 +1,13 @@
 import "./index.css";
 
 import { Testemonial, About, Footer, Header, Skils, Work } from "./container";
+import { Navbar } from "./components/index";
 
+import "./App.scss";
 export function App() {
   return (
     <div className="app">
+      <Navbar />
       <Header />
       <About />
       <Work />
