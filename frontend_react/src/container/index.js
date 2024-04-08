c8927e6e061670421924c48ab04bd77f12a2b832
Maia <matteusmaia889@gmail.com>
2024-04-08 12:54:04 -0300

    indexs to import

---
+++ frontend_react/src/container/index.js
@@ -0,0 +1,6 @@
+export { default as About } from "./About/About";
+export { default as Footer } from "./Footer/Footer";
+export { default as Skils } from "./Skills/Skills";
+export { default as Testemonial } from "./Testemonial/Testemonial";
+export { default as Work } from "./Work/Work";
+export { default as HeaderApp } from "./HeaderApp/HeaderApp";
