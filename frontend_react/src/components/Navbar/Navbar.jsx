2931e09d649cd9d19e3b2b90c5b6efbfe4f8ddc7
Maia <matteusmaia889@gmail.com>
2024-04-07 21:37:14 -0300

    feat: base of Web Navbar

--- frontend_react/src/components/Navbar/Navbar.jsx
+++ frontend_react/src/components/Navbar/Navbar.jsx
@@ -3,10 +3,20 @@ import "./Navbar.scss";
 import { images } from "../../constants";
 function Navbar() {
   return (
-    <nav>
-      <div>
+    <nav className="app__navbar">
+      <div className="app__navbar-logo">
         <img src={images.logo} alt="logo" />
       </div>
+      <ul className="app__navbar-links">
+        {["home", "about", "contact", "work", "skills", "contact"].map(
+          (item) => (
+            <li key={`link-${item}`} className="app__flex p-text">
+              <div />
+              <a href={`#${item}`}>{item}</a>
+            </li>
+          )
+        )}
+      </ul>
     </nav>
   );
 }
